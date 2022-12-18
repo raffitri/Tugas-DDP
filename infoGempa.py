69dc8936f4a5cdf4b9c195b6ef7a7da0b1ca9fe5
raffitri <114563097+raffitri@users.noreply.github.com>
2022-12-18 19:28:49 +0700

    Add files via upload

---
+++ infoGempa.py
@@ -0,0 +1,13 @@
+from Gempa import *
+#membuat object
+g1 = Gempa('Bali',1.2)
+g2 = Gempa('Cinajur',6.7)
+g3 = Gempa('Sumendang',4.8)
+g4 = Gempa('Malang',2.6)
+g5 = Gempa('Banten', 8.2)
+#gunakan member class
+g1.dampak()
+g2.dampak()
+g3.dampak()
+g4.dampak()
+g5.dampak()
